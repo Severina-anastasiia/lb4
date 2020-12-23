e7a1b4b714f8c9a109a872c94a8f5a14997aadac
Severina-anastasiia <severina.anastasiia@nure.ua>
2020-12-23 17:08:28 +0200

    first commit

---
+++ test.php
@@ -0,0 +1,18 @@
+<<body>
+Тест 1:
+<?php
+echo "Hello, World!<br>"
+?>
+Тест 2:
+<?php echo 2+2 ?>
+
+
+<br>
+<form action = "vote.php" method="POST">
+<b>Какой ваш любимый язык программирования?</b><p>
+<input type="Radio" name=otvet value=0 checked>С++<br>
+<input type="Radio" name=otvet value=1>PHP<br>
+<input type="Radio" name=otvet value=2>Java<p>
+<input type="Submit" name=vote value=" Голосовать! "><p>
+</form>
+
